89135ae0c5aaa01cff2cb404a7b3bcbc63e04852
Kiara Jeff <ki.mij@icloud.com>
2023-04-17 19:38:01 +0000

    final

--- class_person.rb
+++ class_person.rb
@@ -5,9 +5,22 @@
 #  - An attribute birthdate (we just assign a string to this attribute, e.g. "April 19, 1987")
 #  - An instance method, full_name, that puts first_name and last_name together
 #  - An instance method, age, that calculates the number of years between today and birthdate
+require 'date'
 
 class Person
-  
+  attr_accessor :first_name, :last_name, :birthdate
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    birthdate_date = Date.parse(birthdate)
+    now = Date.today
+    age_in_days = now - birthdate_date
+    age_in_years = age_in_days / 365
+    age_in_years.to_i
+  end
 end
 
 # Test your instance methods by adding to the end of this file:
@@ -24,3 +37,12 @@ end
 #  other_person.age
 # Output:
 #=>  45
+
+new_person = Person.new
+new_person.first_name = "Joe"
+new_person.last_name = "Mama"
+puts new_person.full_name 
+
+other_person = Person.new
+other_person.birthdate = "April 19, 1987"
+puts other_person.age
